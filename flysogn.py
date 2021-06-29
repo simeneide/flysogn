6e751cab537fb6d626de6bc4df29af575c7f2acb
Simen Eide <simen@eide>
2021-06-29 15:28:18 +0200

    add text report

--- flysogn.py
+++ flysogn.py
@@ -1,7 +1,9 @@
 #%% 
+from numpy.core.numeric import Inf
 import streamlit as st
 import requests
 import pandas as pd
+import datetime
 from metar import Metar
 import plotly.graph_objects as go
 
@@ -42,11 +44,21 @@ df = pd.DataFrame(obs_list)
 #%% Presentation
 st.title("Flyinfo Storhogen")
 last_obs = obs_list[-1]
+col1, col2 = st.beta_columns(2)
+
+
+time_since_last = datetime.datetime.now()-last_obs['time']
+col1.markdown(f"""
+**time:** \t {last_obs['time']}  
+**Hours since reading **: \t {time_since_last.seconds/3600:.1f}  
+speed:  \t {last_obs['wind_speed']} m/s  
+direction: \t {last_obs['wind_dir']}°
+""")
+
 fig = go.Figure(go.Barpolar(
     r=[last_obs['wind_speed']],
     theta=[last_obs['wind_dir']],
     width=[20],
-    #marker_color=["#E4FF87"],
     marker_line_color="black",
     marker_line_width=2,
     opacity=0.8
@@ -63,20 +75,20 @@ fig.update_layout(
       )
     )
 )
-st.plotly_chart(fig)
+col2.plotly_chart(fig)
 #%% Historical data
 fig = go.Figure(data=go.Scatter(x=df.time, y=df.wind_dir))
 fig.update_layout(title="Vindretning storhogen", xaxis_title="Tid", yaxis_title="Vindretning [°]")
 st.plotly_chart(fig)
 
 fig = go.Figure(data=go.Scatter(x=df.time, y=df.wind_speed))
-fig.update_layout(title="Vindstyrke storhogen", xaxis_title="Tid", yaxis_title="Vind [m/s]")
+fig.update_layout(title="Vindstyrke storhogen", xaxis_title="Tid", yaxis_title="Vind [m/s]", autosize=True)
 st.plotly_chart(fig)
 
 #%% WINDY
 st.subheader("Windy nå 1500moh")
 st.components.v1.html("""
-<iframe width="650" height="450" src="https://embed.windy.com/embed2.html?lat=61.010&lon=7.015&detailLat=61.249&detailLon=7.086&width=650&height=450&zoom=8&level=850h&overlay=wind&product=ecmwf&menu=&message=true&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=true&metricWind=m%2Fs&metricTemp=%C2%B0C&radarRange=-1" frameborder="0"></iframe>
+<iframe width="100%" height="450" src="https://embed.windy.com/embed2.html?lat=61.010&lon=7.015&detailLat=61.249&detailLon=7.086&width=650&height=450&zoom=8&level=850h&overlay=wind&product=ecmwf&menu=&message=true&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=true&metricWind=m%2Fs&metricTemp=%C2%B0C&radarRange=-1" frameborder="0"></iframe>
 """, height=450)
 
 st.text("""
