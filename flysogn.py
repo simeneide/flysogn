a9f0c2b749c6b150196c778fb60f76ef4462fe23
simen <simeneide@gmail.com>
2022-03-25 16:58:59 +0100

    add map with netatmo readings

--- flysogn.py
+++ flysogn.py
@@ -1,12 +1,19 @@
 #%% 
+from http import client
 from numpy.core.numeric import Inf
 import streamlit as st
 import requests
 import pandas as pd
-import datetime
+import numpy as np
+from datetime import datetime
 from metar import Metar
 import plotly.graph_objects as go
 import plotly.express as px
+px.set_mapbox_access_token(st.secrets['mapbox_token'])
+import geopandas as gpd
+import shapely.geometry
+from shapely.affinity import affine_transform as T
+from shapely.affinity import rotate as R
 
 def get_storhogen_data():
     url_metar = "https://api.met.no/weatherapi/tafmetar/1.0/metar.txt?icao=ENSG"
@@ -43,6 +50,93 @@ def get_storhogen_data():
     df = pd.DataFrame(obs_list)
     return df
 
+#%% NETATMO AUTH
+def collect_netatmo_data():
+    # use existing package to fix authentication of netatmo
+    import netatmo
+    ws = netatmo.WeatherStation( {
+            'client_id': st.secrets['netatmo_client_id'],
+            'client_secret': st.secrets['netatmo_client_secret'],
+            'username': st.secrets['netatmo_username'],
+            'password': st.secrets['netatmo_password'],
+            'device': st.secrets['netatmo_device']} )
+    ws.get_data()
+    #%% NETATMO
+    # lon / lat
+    coord_sw = [6.4635085,61.0616599]
+    coord_ne = [6.76,61.646145]
+
+    r = requests.get(f"https://api.netatmo.com/api/getpublicdata?lat_ne={coord_ne[1]}&lon_ne={coord_ne[0]}&lat_sw={coord_sw[1]}&lon_sw={coord_sw[0]}&required_data=wind&filter=true",  headers={'Authorization': f'Bearer {ws._access_token}'})
+    import json
+    msg = json.loads(r.content)
+    if r.status_code!=200: 
+        print(r.content)
+    d = msg['body'][1]
+
+    ## Organize data into dataframe
+    data = []
+    for d in msg['body']:
+        obs = {}
+        obs['lat'] = d['place']['location'][1]
+        obs['lon'] = d['place']['location'][0]
+        obs['altitude'] = d['place']['altitude']
+        obs['name'] = f"netatmo-{d['place'].get('street')}"
+        data.append(obs)
+
+        for mac, observations in d['measures'].items():
+            if "wind_angle" in observations.keys():
+                if observations['wind_angle'] >= 0:
+                    for s in ['wind_angle','wind_strength','wind_timeutc','gust_strength']:
+                        obs[s] = observations[s]
+
+    df = pd.DataFrame(data).dropna()
+    df['hours_since_reading'] = np.round(df['wind_timeutc'].apply(lambda epoch: (datetime.utcnow()-datetime.utcfromtimestamp(epoch)).seconds/3600), 1)
+
+    df.head()
+    df['s'] = np.sqrt(df['wind_strength'])/500
+    return df
+
+def plot_wind_arrows(df):
+    a = shapely.wkt.loads(
+        "POLYGON ((-0.6227064947841563 1.890841205238906, -0.3426264166591566 2.156169330238906, -0.07960493228415656 2.129731830238906, 1.952059130215843 0.022985736488906, -0.2085619635341561 -2.182924419761094, -0.6397611822841562 -1.872877544761094, -0.6636088385341563 -1.606053326011095, 0.5862935052158434 -0.400158794761094, -2.312440869784157 -0.3993228572610942, -2.526870557284156 -0.1848931697610945, -2.517313916659156 0.2315384708639062, -2.312440869784157 0.3990052677389059, 0.5862935052158434 0.399841205238906, -0.6363314947841564 1.565763080238906, -0.6227064947841563 1.890841205238906))"
+    )
+    # scatter points
+    t = (
+        px.scatter_mapbox(df, lat="lat", lon="lon", 
+        color="wind_strength", hover_data=["wind_strength","wind_angle","hours_since_reading","name","altitude"])
+        .update_layout(mapbox={"style": "carto-positron"})
+        .data
+    )
+
+    # wind direction and strength
+    fig = px.choropleth_mapbox(
+        df,
+        geojson=gpd.GeoSeries(
+            df.loc[:, ["lon", "lat", "wind_angle", "s"]].apply(
+                lambda r: R(
+                    T(a, [r["s"], 0, 0, r["s"], r["lon"], r["lat"]]),
+                    angle=-90-r['wind_angle'],
+                    origin=(r["lon"], r["lat"]),
+                    use_radians=False,
+                ),
+                axis=1,
+            )
+        ).__geo_interface__,
+        locations=df.index,
+        color="wind_strength",
+        opacity=0.5
+    )
+    fig.add_traces(t)
+    fig.update_layout(
+        mapbox={
+            "style": "carto-positron", 
+            "zoom": 8,
+                    "center":dict(
+                lat=df['lat'].mean(),
+                lon=df['lon'].mean()
+            )}, margin={"l":0,"r":0,"t":0,"b":0})
+    return fig
+#%%
 
 #%% Presentation
 st.title("Flyinfo Sogn")
@@ -62,10 +156,10 @@ try:
     
     df_storhogen = get_storhogen_data()
     last_obs = df_storhogen.tail(1).to_dict("records")[0]
-    time_since_last = datetime.datetime.now()-last_obs['time']
+    hours_since_reading = datetime.now()-last_obs['time']
     st.markdown(f"""
     **time:** \t {last_obs['time']}  
-    **Hours since reading **: \t {time_since_last.seconds/3600:.1f}  
+    **Hours since reading **: \t {hours_since_reading.seconds/3600:.1f}  
     speed:  \t {last_obs['wind_speed']:.1f} m/s  
     direction: \t {last_obs['wind_dir']}°
     """)
@@ -93,6 +187,10 @@ fig.update_layout(
 )
 st.plotly_chart(fig)
 
+st.subheader("Netatmo")
+df = collect_netatmo_data()
+fig = plot_wind_arrows(df)
+fig
 
 
 #%% WINDY
@@ -105,12 +203,6 @@ st.components.v1.iframe(
 #%% Vindnå.no
 st.components.v1.iframe(
     src="https://vindnå.no",
-    height=450
-)
-
-#%% NETATMO
-st.components.v1.iframe(
-    src="https://weathermap.netatmo.com/?zoom=10&type=wind&param=wind&maplayer=Map&lat=61.30988839048675&lng=7.133899930132202&lang=en",
     height=450
 )
 
