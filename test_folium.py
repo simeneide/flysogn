c17bc4c650ae83abd7098e2efb4b0aac260178fe
simen <simeneide@gmail.com>
2024-06-16 13:49:37 +0200

    add marker

--- test_folium.py
+++ test_folium.py
@@ -3,6 +3,10 @@ from streamlit_folium import st_folium
 import streamlit as st
 m = folium.Map(location=[61.22908, 7.09674], zoom_start=9, tiles="openstreetmap")
 m.add_child(folium.LatLngPopup())
+
+marker = folium.Marker(location=[61.22908, 7.09674], popup='Marker Popup Text')
+m.add_child(marker)
+
 map = st_folium(m,width="50%")
 
 if map['last_clicked'] is not None:
