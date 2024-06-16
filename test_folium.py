a4523d44aeac8e4fbbec0bad38fa053bc1119cc2
simen <simeneide@gmail.com>
2024-06-16 13:44:37 +0200

    make lightweight app to test st_folium

---
+++ test_folium.py
@@ -0,0 +1,9 @@
+import folium
+from streamlit_folium import st_folium
+import streamlit as st
+m = folium.Map(location=[61.22908, 7.09674], zoom_start=9, tiles="openstreetmap")
+m.add_child(folium.LatLngPopup())
+map = st_folium(m,width="50%")
+
+if map['last_clicked'] is not None:
+    st.write(map['last_clicked']['lat'],map['last_clicked']['lng'])
