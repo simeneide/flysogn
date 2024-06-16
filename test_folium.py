576552b51a4bd87d688917af980cff833617796d
simen <simeneide@gmail.com>
2024-06-16 13:52:01 +0200

    put inside expand

--- test_folium.py
+++ test_folium.py
@@ -1,13 +1,14 @@
 import folium
 from streamlit_folium import st_folium
 import streamlit as st
-m = folium.Map(location=[61.22908, 7.09674], zoom_start=9, tiles="openstreetmap")
-m.add_child(folium.LatLngPopup())
+with st.expander("Map", expanded=True):
+    m = folium.Map(location=[61.22908, 7.09674], zoom_start=9, tiles="openstreetmap")
+    m.add_child(folium.LatLngPopup())
 
-marker = folium.Marker(location=[61.22908, 7.09674], popup='Marker Popup Text')
-m.add_child(marker)
+    marker = folium.Marker(location=[61.22908, 7.09674], popup='Marker Popup Text')
+    m.add_child(marker)
 
-map = st_folium(m,width="50%")
+    map = st_folium(m,width="50%")
 
-if map['last_clicked'] is not None:
-    st.write(map['last_clicked']['lat'],map['last_clicked']['lng'])
+    if map['last_clicked'] is not None:
+        st.write(map['last_clicked']['lat'],map['last_clicked']['lng'])
